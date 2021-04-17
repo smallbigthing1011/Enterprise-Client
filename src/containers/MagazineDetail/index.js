08572c85ea23f087f9290528fb92ac0e6d82ce0f
smallbigthing1011 <smallbigthing1011@gmail.com>
2021-04-17 15:15:46 +0700

    fix MagazineDetail indx

--- src/containers/MagazineDetail/index.js
+++ src/containers/MagazineDetail/index.js
@@ -1,31 +1,13 @@
-import { Box, Button, Grid, makeStyles, Typography } from "@material-ui/core";
-import AddRoundedIcon from "@material-ui/icons/AddRounded";
-import MenuIcon from "@material-ui/icons/Menu";
-import React, { useState } from "react";
+import React from "react";
+import { Grid, Box, Typography, Button, makeStyles } from "@material-ui/core";
+import { SideBar, ContributionsTable } from "../../components";
 import { Link } from "react-router-dom";
-import { ContributionsTable, SideBar } from "../../components";
-
+import AddRoundedIcon from "@material-ui/icons/AddRounded";
 const useStyles = makeStyles({
-  sidebarOpen: {
+  sidebar: {
     backgroundColor: "#00587A",
     position: "fixed",
     height: "100vh",
-    transition: "ease-in 0.5s",
-  },
-  sidebarClose: {
-    height: "0",
-  },
-  menuBtnOpen: {
-    height: "30px",
-    color: "#fff",
-    position: "fixed",
-    zIndex: "999",
-  },
-  menuBtnClose: {
-    height: "30px",
-    color: "#000000",
-    position: "fixed",
-    zIndex: "999",
   },
   main: {
     minHeight: "100vh",
@@ -51,49 +33,32 @@ const useStyles = makeStyles({
   },
 });
 const MagazineDetail = () => {
-  const [close, setClose] = useState(false);
   const classes = useStyles();
-  const handleClick = () => {
-    setClose(!close);
-  };
+
   return (
     <div>
-      <Button
-        className={close ? classes.menuBtnClose : classes.menuBtnOpen}
-        onClick={handleClick}
-      >
-        <MenuIcon></MenuIcon>
-      </Button>
       <Grid container>
         <Grid
           item
           container
           justify="center"
-          xs={4}
-          sm={4}
-          md={4}
-          lg={4}
-          className={close ? classes.sidebarClose : classes.sidebarOpen}
+          md={3}
+          className={classes.sidebar}
         >
-          {close ? "" : <SideBar rolebase="admin"></SideBar>}
+          <SideBar rolebase="admin"></SideBar>
         </Grid>
-
+        <Grid item md={3}></Grid>
         <Grid
           container
           justify="space-between"
           alignItems="start"
           direction="column"
           item
-          xs={12}
-          sm={12}
-          md={12}
-          lg={12}
+          md={9}
           className={classes.main}
         >
           <Box className={classes.title}>
-            <Typography variant="h5" align="right">
-              {2021}
-            </Typography>
+            <Typography variant="h5">{2021}</Typography>
           </Box>
           <Box className={classes.table}>
             <Box>
